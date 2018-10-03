e47693ed018efe0e2a3e90c1b66d77c105a64c66
Alvin Keyser <alvinck55@gmail.com>
2018-10-03 14:44:48 -0500

    Adding one test case example

--- src/test/java/shuffleTest.java
+++ src/test/java/shuffleTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class shuffleTest {
+
+    @Test
+    public void shuffleTest(){
+        int deckSize = 42;
+        assertEquals(Risk.shuffleDeck().size(), deckSize);
+    }
 }
