b0195fc258bc09f0c343aa5a123569c30f9e47bc
ethan.y.hsiao@gmail.com <ethan.y.hsiao@gmail.com>
2018-10-04 13:57:10 -0500

    adding test case

---
+++ src/test/java/getCardTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class getCardTest {
+
+    @Test
+    public void getCardTest(){
+        int sampleCard1= 1, sampleCard2= 2, sampleCard3= 3;
+        ArrayList<Integer> list= new ArrayList <> (Arrays.asList(sampleCard1, sampleCard2, sampleCard3));
+        assertEquals(Risk.getCard(list), 1);
+        assertEquals(Risk.getCard(list), 2);
+        assertEquals(Risk.getCard(list), 3);
+    }
+}
