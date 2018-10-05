2b280ad6ff6a64b7d534383a40fe30d44dbf3806
ethan.y.hsiao@gmail.com <ethan.y.hsiao@gmail.com>
2018-10-04 21:18:49 -0500

    update

---
+++ src/test/java/createTerritoryTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class createTerritoryTest {
+
+    @Test
+    public void createTerritoryTest(){
+        ArrayList<territory> territories = new ArrayList<>();
+        int totalTerritories = 42;
+        assertEquals(Risk.createTerritories(territories).size(), totalTerritories);
+    }
+}
